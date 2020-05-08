99ddb50d150d4671839cda60f47b59de3068468d
ark <arkadii@darly.solutions>
2020-05-08 16:09:51 +0300

    caching

--- app/controllers/tickets/tickets_controller.rb
+++ app/controllers/tickets/tickets_controller.rb
@@ -1,11 +1,11 @@
 class Tickets::TicketsController < ApplicationController
   def index
     @pagy, @records = pagy(Ticket.all)
-    render 'tickets/index', locals: { tickets: @records }
+    render 'tickets/index', locals: { tickets: @records } if stale?(@records)
   end
 
   def show
     @record = Ticket.find(params[:id])
-    render 'tickets/show', locals: { ticket: @record }
+    render 'tickets/show', locals: { ticket: @record } if stale?(@record)
   end
 end
